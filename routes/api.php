196fc361bb7bf780f6a1bb78ac353e510aaedd90
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-07-18 22:08:30 +0700

    add our first api endpoint

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\api\v1\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/task', TaskController::class);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
