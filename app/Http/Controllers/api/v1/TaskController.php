4b06eac7b64abd11be519330da03b21c2347d0d7
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-07-19 09:39:41 +0700

    add a post request endpoint to add a new task

--- app/Http/Controllers/api/v1/TaskController.php
+++ app/Http/Controllers/api/v1/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
@@ -20,9 +21,12 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTaskRequest $request)
     {
-        //
+        $task = Task::create($request->validated());
+
+        return TaskResource::make($task);
+ 
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\api\v1\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('v1')->group(function () {
+    Route::get('/task', [TaskController::class, 'store']);
 });
